cddd52ded96577c8624811600f84992482de8879
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2019-03-18 13:42:08 -0300

    Criado o método de inserir atividade

--- KanbanProjeto/Controllers/AtividadeController.cs
+++ KanbanProjeto/Controllers/AtividadeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Controllers.Base;
+using Controllers.DAO;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,20 @@ using System.Threading.Tasks;
 
 namespace Controllers
 {
-    public class AtividadeController
+    public class AtividadeController : IBaseController<Atividade>
     {
+        Contexto contexto = new Contexto();
+
+        public int inserirAtividade(Atividade atividade)
+        {
+            if(atividade != null)
+            {
+                contexto.Atividades.Add(atividade);
+                contexto.SaveChanges();
+                return 1;
+            }
+
+            return 0;
+        }
     }
 }
